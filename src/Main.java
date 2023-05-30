5621df91d1bcc4acadb5e091bce65e8538dfebc4
unknown <cburgosn15@gmail.com>
2023-05-30 16:26:40 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        //Enteros
+        byte entero_byte = 1;
+        short entero_short = 100;
+        int entero_int = 10000;
+        long entero_long = 10000000;
+        System.out.println("Enteros");
+        System.out.println("byte: " + entero_byte);
+        System.out.println("short: "+ entero_short);
+        System.out.println("int: "+ entero_int);
+        System.out.println("long: "+ entero_long);
+        System.out.println("---------------------------------");
+
+        //Decimales
+        float decimal_float = 4.01f;
+        double decimal_double = 25230.50d;
+        System.out.println("Decimales");
+        System.out.println("float: " + decimal_float);
+        System.out.println("double: "+ decimal_double);
+        System.out.println("---------------------------------");
+
+        //Booleanos
+        boolean bool = false;
+        System.out.println("Booleanos");
+        System.out.println("boolean: " + bool);
+        System.out.println("---------------------------------");
+
+        //Texto
+        char texto_char = 'a';
+        String texto_string = "Hola";
+        System.out.println("Texto");
+        System.out.println("texto_char: " + texto_char);
+        System.out.println("texto_string: " + texto_string);
+        System.out.println("---------------------------------");
+    }
+}
